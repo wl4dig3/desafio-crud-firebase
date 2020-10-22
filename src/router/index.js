a4396038c513cb876a4d9181125dec9ec7ef4e0f
Wladimir Guevara <wladige@gmail.com>
2020-10-21 23:24:01 -0300

    creando proyecto

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/edita',
+    name: 'Edita',
+    component: () => import('../views/Edita.vue')
+  },
+  {
+    path: '/agrega',
+    name: 'Agrega',
+    component: () => import('../views/Agrega.vue')
+  }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
