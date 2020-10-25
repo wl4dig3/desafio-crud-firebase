7e93a8632976a6f365c118db5804b99990ef5a3b
Wladimir Guevara <wladige@gmail.com>
2020-10-24 23:14:28 -0300

    margen top a logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <div class="text-center">
+    <div class="text-center mt-5">
       <router-link :to="{name: 'Home'}">
         <img src="@/assets/tareas.jpg" alt="">
       </router-link>
